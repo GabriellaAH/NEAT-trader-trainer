26c2d4497dc63f6172cfe70d25d2bcc80cde2630
GabriellaAH <gabi.a.herke@gmail.com>
2022-06-11 20:00:08 +0100

    Updated logic

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 
 This simple example code, was developed to show how we can use a NEAT algorithm to train a trading AI.
-To achieve this goal, this AI uses 23 input neurons and 48 hidden neurons and only one output neuron. 
+To achieve this goal, this AI uses 22 input neurons and 11 hidden neurons and only one output neuron. 
 The activation function is tanh. 
 When the output neurons value over 0.5 and we don't have an open positions buy a bitcoin. 
-Pupulation level: 50 trader
+Pupulation level: 100 trader
 ## buy parameters:
 - Risk-Rewar: 1:1.5 
 - Stop lost level: 2 times ATR (pos 14)
 - Take profit level 3 times ATR
 
 ## Input parameters:
-- last 20 close price
-- price over EMA 20
-- price over EMA 200
+- last 10 close price
+- last 10 SMA 20 / price ratio
+- last SMA 200 / price ratio
 
 Each run try to trade for 3 years on Bitcoin. When this period is over, the best trader mutating and creates a new population from the best trader genom. 
--- neat-trader-trainer.py
+++ neat-trader-trainer.py
@@ -7,25 +7,23 @@ from finta import TA
 gen = 0
 idx = 0
 df = pd.read_csv('BTC_19_22_5m.csv', index_col="startTime")
-df['ema200'] = TA.EMA(df, period=200)
-df['ema20'] = TA.EMA(df, period=20)
+df['ema200'] = TA.SMA(df, period=200)
+df['ema20'] = TA.SMA(df, period=20)
 df['atr'] = TA.ATR(df)
 index_labels = df.index.tolist()
 
 class Trade:
     def __init__(self) -> None:
         self.haveposition = False
-        self.openprice = 0
         self.tp = 0
         self.sl = 0
         self.win = 0
         self.lost = 0
         self.num_of_trade = 0
         
-    def buy(self, price, tp, sl) -> None:
+    def buy(self, tp, sl) -> None:
         if not self.haveposition:
             self.haveposition = True
-            self.openprice = price
             self.tp = tp 
             self.sl = sl
             self.num_of_trade += 1
@@ -40,14 +38,12 @@ class Trade:
             return 0
         if self.sl > low:
             self.haveposition = False
-            self.openprice = 0
             self.tp = 0
             self.sl = 0
             self.lost += 1
             return 0
         if self.tp < high:
             self.haveposition = False
-            self.openprice = 0
             self.tp = 0
             self.sl = 0
             self.win += 1
@@ -83,70 +79,47 @@ def eval_genomes(genomes, config):
     
     while run and len(df)-10 > pos:
         pos += 1
-        pipe_ind = 0
 
         st_p = df.loc[index_labels[pos-20], 'open']
-        c1 = df.loc[index_labels[pos-19], 'close']/st_p
-        c2 = df.loc[index_labels[pos-18], 'close']/st_p
-        c3 = df.loc[index_labels[pos-17], 'close']/st_p
-        c4 = df.loc[index_labels[pos-16], 'close']/st_p
-        c5 = df.loc[index_labels[pos-15], 'close']/st_p
-        c6 = df.loc[index_labels[pos-14], 'close']/st_p
-        c7 = df.loc[index_labels[pos-13], 'close']/st_p
-        c8 = df.loc[index_labels[pos-12], 'close']/st_p
-        c9 = df.loc[index_labels[pos-11], 'close']/st_p
-        c10 = df.loc[index_labels[pos-10], 'close']/st_p
-        c11 = df.loc[index_labels[pos-9], 'close']/st_p
-        c12 = df.loc[index_labels[pos-8], 'close']/st_p
-        c13 = df.loc[index_labels[pos-7], 'close']/st_p
-        c14 = df.loc[index_labels[pos-6], 'close']/st_p
-        c15 = df.loc[index_labels[pos-5], 'close']/st_p
-        c16 = df.loc[index_labels[pos-4], 'close']/st_p
-        c17 = df.loc[index_labels[pos-3], 'close']/st_p
-        c18 = df.loc[index_labels[pos-2], 'close']/st_p
-        c19 = df.loc[index_labels[pos-1], 'close']/st_p
-        c20 = df.loc[index_labels[pos], 'close']/st_p
         atr = df.loc[index_labels[pos], 'atr']
-        e20 = 0
-        e200 = 0
-        if df.loc[index_labels[pos], 'close'] > df.loc[index_labels[pos], 'ema20']:
-            e20 = 1
-        else:
-            e20 = 0
-        if df.loc[index_labels[pos], 'close'] > df.loc[index_labels[pos], 'ema200']:
-            e200 = 1
-        else:
-            e200 = 0
-        price =  df.loc[index_labels[pos], 'close']
+        e200 = df.loc[index_labels[pos], 'ema200'] / df.loc[index_labels[pos], 'close'] 
+
+        price = df.loc[index_labels[pos], 'close']
         sl = price - (2*atr)   
         tp = price + (3 * atr)            
         ttp = tp / st_p
+
+        neurons = (ttp, e200, )
+        for i in range (pos, pos-10, -1):
+            if (df.loc[index_labels[i], 'high'] > df.loc[index_labels[i], 'ema20'] and \
+                df.loc[index_labels[i], 'low'] > df.loc[index_labels[i], 'ema20']) or \
+            (df.loc[index_labels[i], 'high'] < df.loc[index_labels[i], 'ema20'] and \
+                df.loc[index_labels[i], 'low'] < df.loc[index_labels[i], 'ema20']):
+                s = df.loc[index_labels[i], 'ema20'] / df.loc[index_labels[i], 'close']
+            else:
+                s = 0
+            c = df.loc[index_labels[i], 'close']/st_p
+            neurons = neurons + (s, c,)          
 
         idx = pos
         for x, trd in enumerate(trades):  
             i = trd.step()
             if trd.num_of_trade > 10:
-                ge[x].fitness = (trd.winrate() * trd.num_of_trade * 1.5) - trd.lost
+                ge[x].fitness = (trd.win *  1.5) - trd.lost
             else:
                 ge[x].fitness = 0
 
             
-            output = nets[trades.index(trd)].activate(
-                (c1, c2, c3, c4, c5, 
-                 c6, c7, c8, c9, c10, 
-                 c11, c12, c13, c14, c15, 
-                 c16, c17, c18, c19, c20, 
-                 e20, e200, ttp)
-            )
+            output = nets[trades.index(trd)].activate(neurons)
+
 
             if output[0] > 0.5:  # tanh activation function result will be between -1 and 1. if over 0.5 buy
-
-                trd.buy(price, tp, sl)
+                trd.buy(tp, sl)
 
             if pos%1000 == 0:
-                print('Pos: %s, agent: %s Fitness: %s, trades: %.2f, winrate: %.2f' % (pos, x, ge[x].fitness, trd.num_of_trade, trd.winrate()))
+                print('Pos: %s, agent: %s Fitness: %s, trades: %.2f, winrate: %.2f' % (pos, x, ge[x].fitness, trd.num_of_trade, trd.winrate()*100))
                 
-        if pos%10000 == 0:
+        if pos%5000 == 0:
             old_trades = trades
             for trd in old_trades:
                 if trd.num_of_trade == 0:
